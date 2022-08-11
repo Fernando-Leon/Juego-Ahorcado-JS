7acc8b9506b12bb5c56988cc7906d6bcd09b89e8
Fernando-Leon <leon.monroy.fernando@gmail.com>
2022-08-11 09:02:02 -0500

    New Rowdaline

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,37 @@
 const listPharases = ["Hello world", "Java Script", "School Garden", "Love Programming", "Ingenier Developer", "Space X", "Elon Musk", "Indra Li", "I Love You Li"];
-const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
 
-const generatePharase = () => {
-    let pharaseContent = document.getElementById("container-string"); 
-    let pharase = listPharases[Math.floor(Math.random() * (listPharases.length - 0) + 0)];
-    let fragmentMain = document.createDocumentFragment();
+const generateZonePlay = () => {
+    let containerPhrase = document.getElementById("container-string");
+    const phrase = listPharases[Math.floor(Math.random() * (listPharases.length - 1) + 1)];
+    let fragment = document.createDocumentFragment();
 
-    for(let i = 0; i < pharase.length; i++) {
-        let itemCharacter = document.createElement("DIV");
-        itemCharacter.setAttribute("class", "div-character");
-        (pharase[i] === " ") ? itemCharacter.innerHTML = "-": itemCharacter.innerHTML = pharase[i]; 
-        fragmentMain.appendChild(itemCharacter);
+    for(let i = 0; i < phrase.length; i++){
+        let itemChar = document.createElement("DIV");
+        itemChar.innerHTML = " ";
+        fragment.appendChild(itemChar);
     }
+    containerPhrase.appendChild(fragment);
 
-    pharaseContent.appendChild(fragmentMain);
+    containerPhrase.style.gridTemplateColumns = `repeat(${phrase.length}, 1fr)`;
 
-    let numDiv = document.getElementsByClassName("div-character").length;
-    document.getElementById("container-string").style.gridTemplateColumns = `repeat(${numDiv}, 1fr)`;
+    return console.log("La palabra es"+phrase);
 }
 
-const generateAbc = () =>{
-    let containerAbc = document.getElementById("abc-container");
-    let fragmetAbc = document.createDocumentFragment();
-    for(let i = 0; i < abc.length; i++){
-        let containerDiv = document.createElement("div");
-        containerDiv.setAttribute("class", "conatiner-char")
-        let itemChar = document.createElement("div");
-        itemChar.setAttribute("class", "char");
-        itemChar.innerHTML = abc[i];
+const checkToChart = (value) => {
+    alert(`Seleccionaste la letra: ${value}`);
+}
+//Logic Buttons 
 
-        containerDiv.appendChild(itemChar);
-        fragmetAbc.appendChild(containerDiv);
+//Button to: inital play
+var statusInital = true;
+const inital = () => {
+    if(statusInital){
+        generateZonePlay();
+        document.getElementById("btn-inital").style.visibility = 'hidden';
+        statusInital = false;
+    }else{
+        document.getElementById("btn-inital").style.visibility = 'hidden';
     }
-    containerAbc.appendChild(fragmetAbc);
-} 
-generateAbc();
+}
+
+//https://drive.google.com/file/d/182TswNCEcrtWvnU9Itn-wscvyy-HfKm1/view
